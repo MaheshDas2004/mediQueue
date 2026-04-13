4b762aecdbe4f7a5eb4ab06a56f9a6e210b409ff
Mahesh Das <mahesh.das2205@gmail.com>
2026-04-13 20:05:07 +0530

    feat: enhance patient model and registration process with additional fields and priority scoring logic

--- backend/app/routes/patient_routes.py
+++ backend/app/routes/patient_routes.py
@@ -2,12 +2,13 @@ from fastapi import APIRouter, Depends,status
 from app.services.patient_service import register_patient,show_patients
 from app.schemas.patient_schema import PatientSchema
 from app.database.connection import get_db
+from app.dependencies.auth import require_triage
 
 patient_router = APIRouter(prefix="/patients")
 
 
 @patient_router.post("/register",status_code=status.HTTP_201_CREATED)
-def register_patient_endpoint(patient_data:PatientSchema, db=Depends(get_db)):
+def register_patient_endpoint(patient_data:PatientSchema,triage_user=Depends(require_triage), db=Depends(get_db)):
     
     return register_patient(patient_data, db)
     
